71d8fd25d5434e4940e4ac1608760552e0b11561
Amy Fogelman <abfnj1@gmail.com>
2019-05-27 01:03:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,16 @@ def (num)
  end
   "HAPPY NEW YEAR!"
 end
+def countdown_with_sleep(num)
+
+ while num > 0
+
+   puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+
+ end
+  "HAPPY NEW YEAR!"
+ end	
 
 end
